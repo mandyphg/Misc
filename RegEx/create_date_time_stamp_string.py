184eec2ec4b92d7d323248def0c93b370e200943
mandyphg <44054536+mandyphg@users.noreply.github.com>
2021-05-31 13:41:41 +0800

    Update create_date_time_stamp_string.py

--- RegEx/create_date_time_stamp_string.py
+++ RegEx/create_date_time_stamp_string.py
@@ -1,5 +1,10 @@
 import re
 import datetime
 
-time_stamp = re.sub('[^A-Za-z0-9]+','',str(datetime.datetime.now()))
-print(times_stamp)
+
+time_now = str(datetime.datetime.now())
+print(time_now)
+
+# remove all characters except numeric and alphabet
+time_stamp = re.sub('[^A-Za-z0-9]+','',time_now)
+print(time_stamp)
